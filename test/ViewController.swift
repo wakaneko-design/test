b3dce7e5602bb808fcf15d77671f03f90cd1975a
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-09-16 16:58:48 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Yuto Wakabayashi on 2019/09/16.
+//  Copyright © 2019 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
